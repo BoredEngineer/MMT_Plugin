6aa622be9523045d8066f72da08fc2d4084bd07f
BoredEngineer <v.kuropyatnik@gmail.com>
2016-07-30 20:22:01 +0200

    Added c++ implementation of the PID Controller

--- Source/MMT/Private/MMTPIDController.cpp
+++ Source/MMT/Private/MMTPIDController.cpp
@@ -14,18 +14,57 @@ UMMTPIDController::UMMTPIDController()
 }
 
 
-// Add error measurment into array
-void UMMTPIDController::AddMeasuredError(FPIDErrorItem* PIDErrorItem)
+// Add error measurement into array
+void UMMTPIDController::AddMeasuredError(const FPIDErrorItem& PIDErrorItem)
 {
-	if(PIDErrorItem != NULL)
-	{
-		MeasuredErrors.Insert(PIDErrorItem, 0);
+	MeasuredErrors.Insert(PIDErrorItem, 0);
 		
-		// remove last item in array if we reached desired amount of errors to track
-		if (MeasuredErrors.Num() > NumOfErrorsToStore)
-		{
-			MeasuredErrors.Pop(true);
-		}
+	// remove last item in array if we reached desired amount of errors to track
+	if (MeasuredErrors.Num() > NumOfErrorsToStore)
+	{
+		MeasuredErrors.Pop(true);
 	}
 
+}
+
+//Calculate and return control signal
+float UMMTPIDController::GetControlCoefficient()
+{
+	float Result = 0.0f;
+
+	if (MeasuredErrors.Num() < 3) 
+		return Result;
+
+	// Proportional error is just the last measured error
+	if (ProportionalCoefficient != 0.0f)
+	{
+		Result += ProportionalCoefficient * MeasuredErrors[0].MeasuredValue;
+	}
+
+	// Integral error is a sum of all measured errors multiplied by respective delta time of measurement
+	if (IntegralCoefficient != 0.0f)
+	{
+		float IntegralError = 0.0f;
+
+		for (int32 i = 0; i < MeasuredErrors.Num(); i++)
+		{
+			IntegralError += MeasuredErrors[i].MeasuredValue * MeasuredErrors[i].DeltaTime;
+		}
+
+		Result += IntegralCoefficient * IntegralError;
+	}
+
+	// Derivative error is difference between last two error divided by delta time of last measurement
+	if (DerivativeCoefficient != 0.0f)
+	{
+		Result += DerivativeCoefficient * (MeasuredErrors[0].MeasuredValue - MeasuredErrors[1].MeasuredValue) / MeasuredErrors[0].DeltaTime;
+	}
+
+	return Result;
+}
+
+//Clear array of measured errors
+void UMMTPIDController::ResetController()
+{
+	MeasuredErrors.Empty();
 }
--- Source/MMT/Public/MMTPIDController.h
+++ Source/MMT/Public/MMTPIDController.h
@@ -14,6 +14,12 @@ public:
 	// Sets default values for this component
 	UMMTPIDController();
 
+
+
+	/**
+	* Minimum of 3 errors need to be stored. Storing more errors is necessary for Integral part of the controller
+	* as it corrects systematic errors of measurements taken over time.
+	*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PID Settings", meta = (ClampMin = "3", UIMin = "3"))
 	int32 NumOfErrorsToStore;
 	
@@ -26,9 +32,27 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PID Settings")
 	float DerivativeCoefficient;
 
-	//UFUNCTION(BlueprintCallable, Category = "MMT PID Controller")
-	static void AddMeasuredError(FPIDErrorItem* PIDErrorItem);
+	/**
+	*	Add measurement error and delta time to PID Controller's memory 
+	*	@param PIDErrorItem	measurement error and delta time item
+	*/
+	UFUNCTION(BlueprintCallable, Category = "MMT PID Controller")
+	void AddMeasuredError(const FPIDErrorItem& PIDErrorItem);
+
+	/**
+	*	Get calculated control signal
+	*	@return calculated control signal
+	*/
+	UFUNCTION(BlueprintCallable, Category = "MMT PID Controller")
+	float GetControlCoefficient();
+
+	/**
+	*	Resets PID Controller by deleting all previously measured errors
+	*/
+	UFUNCTION(BlueprintCallable, Category = "MMT PID Controller")
+	void ResetController();
 
 private:
-	TArray<FPIDErrorItem*> MeasuredErrors;
+	UPROPERTY()
+	TArray<FPIDErrorItem> MeasuredErrors;
 };
